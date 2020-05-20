f2584fc29b7a7cdd3fe0ed95837370dd015f8200
Ian Goodnight <goodnight.ianb@gmail.com>
2020-05-20 14:48:52 -0400

    added support for fashion220

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fetch = require('node-fetch');
 const axios = require('axios');
-
-/* GET users listing. */
-router.get('/:email', function(req, res, next) {
-
-	console.log("Params: ", req.params);
-
-	let xhr = new XMLHttpRequest();
-
-	function handler() {
-		console.log("Response Status: ", this.status);
-		console.log("this object: ", this);
-		res.json(JSON.parse(this.responseText));
-
-	}
-
-	const email = req.params.email;
-
-
-	xhr.open("GET", "https://boutsy.com/admin.php?target=RESTAPI&_key=" + process.env.API_KEY + "&_path=profile&_cnd[pattern]=" + email);
-	xhr.setRequestHeader("accept", "application/json");
-	xhr.setRequestHeader("content-type", "application/json");
-	xhr.onload = handler;
-	xhr.send();
-
-});
 
 router.post('/data', async function(req, res) {
 	// Debugging helpscout request
@@ -237,6 +211,109 @@ router.post('/data', async function(req, res) {
 	
 		res.end();
 
+	// BEGIN BULK APOTHECARY HANDLING
+	} else if (mailbox === 'sales@bulkapothecary.com') {
+
+		console.log("Aiming for the BigCommerce API...");
+		let targetUrl = encodeURI('https://api.bigcommerce.com/stores/' + process.env.FF220_STORE_HASH + '/v3/customers?email:in=' + email);
+		console.log("targetURL: ", targetUrl);
+		let profile = await axios.get(targetUrl, 
+			{
+			headers: {
+				'accept': 'application/json',
+				'content-type': 'application/json',
+				'x-auth-client': process.env.F220_X_CLIENT,
+				'x-auth-token': process.env.F220_X_TOKEN
+			}
+		});
+		let profileId;
+		console.log("Debug: ", profile.data);
+		profile.data.data.length !== 0 ? profileId = profile.data.data[0].id: profileId = '';
+		let ordersUrl = encodeURI('https://api.bigcommerce.com/stores/' + process.env.FF220_STORE_HASH + '/v2/orders?customer_id=' + profileId);
+		let orders;
+		if (profileId !== '') {
+			orders = await axios.get(ordersUrl, 
+				{
+				headers: {
+					'accept': 'application/json',
+					'content-type': 'application/json',
+					'x-auth-client': process.env.F220_X_CLIENT,
+					'x-auth-token': process.env.F220_X_TOKEN
+				}
+			});
+		} else {
+			orders = { data: [] };
+		};
+		let dateCreated = '';
+
+		if (profile.data.data.length !== 0) {
+			let rawDate = profile.data.data[0].date_created.split('T')[0].split('-');
+			dateCreated = rawDate[1] + '/' + rawDate[2] + '/' + rawDate[0];
+		} else {
+			dateCreated = 'Not registered';
+		};
+
+		let ordersTotal = BCOrderAggregator(orders.data);
+		let profileLink = '';
+		let profileHref = '';
+
+		if (orders.data.length !== 0) {
+			profileLink = '<a href="https://www.fashion220.net/manage/orders?customerId=' + profileId + '" target="_blank">' + profile.data.data[0].first_name + ' ' + profile.data.data[0].last_name + '</a>'
+			profileHref = 'https://www.fashion220.net/manage/orders?customerId=' + profileId;
+		} else {
+			profileLink = req.body.customer.fname + ' ' + req.body.customer.lname;
+			profileHref = '#';
+		}
+
+		let orderList = listBCOrders(ordersTotal.orderSummary);
+
+		console.log('Profile: ', profile.data);
+		// console.log('Orders: ', orders.data);
+		console.log('Date created: ', dateCreated);
+		console.log('Orders Total: ', ordersTotal);
+
+		let html = '<h4><a href="https://fashion220.net/manage">Fashion 220</a></h4>' +
+					'<div class="c-sb-section c-sb-section--toggle">' +
+						'<div class="c-sb-section__title js-sb-toggle">' +
+							'Profile <i class="caret sb-caret"></i>' +
+						'</div>' +
+						'<div class="c-sb-section__body">' +
+							'<ul class="unstyled">' +
+						  	'<li>' +
+						  		'<strong>' +
+							   		profileLink +
+							 		'</strong>' +
+						  	'</li>' +
+							  '<li>' +
+							  	'Customer since: ' + dateCreated +
+							  '</li>' +
+							  '<li>' +
+							  	'$' + ordersTotal.total + ' lifetime spending' +
+							  '</li>' +
+							  '<li>' +
+							  	'$'  + ordersTotal.refunded + ' lifetime refunds' +
+							  '</li>' +
+					  	'</ul>' +
+					  '</div>' +
+				  '</div>' +
+				  '<div class="c-sb-section c-sb-section--toggle">' +
+				  	'<div class="c-sb-section__title js-sb-toggle">' +
+				  		'<i class="icon-cart icon-sb"></i> Order History (' + '<a href="' + profileHref + '" target="_blank">' + ordersTotal.number + '</a>)' +
+				  		'<i class="caret sb-caret"></i>' +
+				  	'</div>' +
+				  	'<div class="c-sb-section__body">' +
+				  		'<ul class="unstyled">' +
+				  			orderList +
+				  		'</ul>' +
+				  	'</div>' +
+				  '</div>';
+		console.log("HTML: ", html);
+		if (profile.data.data.length !== 0) {
+    	res.send({html: html});			
+		} else {
+			res.send({html: "<h4>Probably a ghost.</h4>"})
+		}
+		res.end();
 	} // End Mailbox "IF"
 });
 
@@ -253,6 +330,25 @@ function listOrders(orders) {
 			break; 
 		}
 	}
+	return list;
+};
+
+function listBCOrders(orders) {
+	let list = '';
+	for (var i = 0; i < orders.length; i++) {
+		let shipped = '';
+		let notes = '';
+		orders[i].shipped !== 'N/A' ? shipped = ' on ' + orders[i].shipped: shipped = '';
+		orders[i].notes !== '' ? notes = '<li>Order notes: ' + orders[i].notes + '</li>' : notes = '';
+
+		let el = '<li>Order #' + orders[i].id + '</li>' +
+		'<li>Order date: ' + orders[i].created + '</li>' +
+		'<li>Order total: $' + orders[i].total + '</li>' +
+		notes +
+		'<li class="divider">Order status ' + orders[i].status + shipped + '</li>';
+		list += el;
+	};
+	console.log('List: ', list);
 	return list;
 };
 
@@ -318,7 +414,7 @@ async function getCategories(details) {
 			cats: primaryLevelCats
 		};
 	} else {
-		formattedResponse.push('Uncategorized');
+		formattedResponse.push('<span class="red">Uncategorized</span>');
 		console.log("FORMATTEDRESPONSE: ", formattedResponse[0]);
 		return {
 			html: formattedResponse[0],
@@ -327,5 +423,44 @@ async function getCategories(details) {
 	}
 
 };
+
+function BCOrderAggregator (orderData) {
+
+	let orderTotal = 0;
+	let refundedTotal = 0;
+	let numberOrders = 0;
+	let recentOrders = [];
+
+	if (orderData.length !== 0) {
+		let reversed = orderData.reverse();
+		reversed.forEach(order => {
+			let recent = {};
+
+			if (recentOrders.length <= 10) {
+				recent.id = order.id;
+				recent.total = Number(order.total_inc_tax);
+				recent.status = order.status;
+				order.staff_notes === '' ? recent.notes = '': recent.notes = order.staff_notes;
+				recent.created = order.date_created.split(' ').slice(0, 4).join(' ');
+				order.date_shipped === '' ? recent.shipped = 'N/A': recent.shipped = order.date_shipped.split(' ').slice(0, 4).join(' ');
+				recentOrders.push(recent);
+			}
+			orderTotal += Number(order.total_inc_tax);
+			refundedTotal += Number(order.refunded_amount);
+		});
+		numberOrders = orderData.length;
+	}
+
+	orderTotal = Math.round(orderTotal);
+	refundedTotal = Math.round(refundedTotal);
+
+	return { 
+		total: orderTotal,
+		number: numberOrders,
+		refunded: refundedTotal,
+		orderSummary: recentOrders
+	};
+
+}
 
 module.exports = router;
